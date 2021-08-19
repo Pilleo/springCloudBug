d17db43851e82173c3dd570b89400554fec41e6e
Leanid Piliptsevich <Leanid.Piliptsevich@amway.com>
2021-08-19 16:53:08 +0300

    Routing function bug

--- src/main/java/com/example/springcloudbug/SpringCloudBugApplication.java
+++ src/main/java/com/example/springcloudbug/SpringCloudBugApplication.java
@@ -1,32 +1,77 @@
 package com.example.springcloudbug;
 
+import com.amazonaws.services.lambda.runtime.events.KinesisEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.function.context.FunctionRegistration;
 import org.springframework.cloud.function.context.FunctionType;
-import org.springframework.cloud.function.context.FunctionalSpringApplication;
+import org.springframework.cloud.function.context.MessageRoutingCallback;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.messaging.Message;
 
 import java.util.function.Function;
 
-@SpringBootConfiguration
+@SpringBootApplication
 public class SpringCloudBugApplication implements ApplicationContextInitializer<GenericApplicationContext> {
-    public static final String FUNCTION = "fucntion";
 
+    public static final String SQS_FUN = "sqs";
+    public static final String KINESIS_FUN = "kinesis";
+
+    /*
+     * You need this main method (empty) or explicit <start-class>example.FunctionConfiguration</start-class>
+     * in the POM to ensure boot plug-in makes the correct entry
+     */
     public static void main(String[] args) {
-        FunctionalSpringApplication.run(SpringCloudBugApplication.class, args);
+        // empty unless using Custom runtime at which point it should include
+        // FunctionalSpringApplication.run(FunctionConfiguration.class, args);
     }
 
     @Override
-    public void initialize(GenericApplicationContext c) {
-        c.registerBean("bugFunctionRegistration", FunctionRegistration.class, () -> function(c));
-        Function<String, String> function = (str) -> str + str;
+    public void initialize(GenericApplicationContext context) {
+        context.registerBean(MessageRoutingCallback.class, () -> new RoutingCallback(context.getBean(ObjectMapper.class)));
 
-        c.registerBean(FUNCTION,Function.class,()-> function);
+        context.registerBean(SQS_FUN, FunctionRegistration.class,
+                () -> new FunctionRegistration<>(sqsEventStringFunction()).type(
+                        FunctionType.from(SQSEvent.class).to(String.class)));
+
+        context.registerBean(KINESIS_FUN, FunctionRegistration.class,
+                () -> new FunctionRegistration<>(kinesisEventStringFunction()).type(
+                        FunctionType.from(KinesisEvent.class).to(String.class)));
+
     }
 
-    private FunctionRegistration<Function<String, String>> function(GenericApplicationContext c) {
-        return new FunctionRegistration<Function<String, String>>(c.getBean(FUNCTION, Function.class))
-                .type(FunctionType.from(String.class).to(String.class).getType());
+    private Function<SQSEvent, String> sqsEventStringFunction() {
+        return (sqsEvent) -> SQS_FUN;
+    }
+
+    private Function<KinesisEvent, String> kinesisEventStringFunction() {
+        return (KinesisEvent kinesisEvent) -> KINESIS_FUN;
+    }
+
+    public static class RoutingCallback implements MessageRoutingCallback {
+        private final ObjectMapper objectMapper;
+
+        public RoutingCallback(ObjectMapper objectMapper) {
+            this.objectMapper = objectMapper;
+        }
+
+        @Override
+        public String functionDefinition(Message<?> messageWithAmountEvent) {
+
+            try {
+                byte[] payload = (byte[]) messageWithAmountEvent.getPayload();
+                JsonNode tree = objectMapper.readTree(payload);
+                String eventSource = tree.findValue("Records").get(0).get("eventSource").asText();
+                if ("aws:sqs".equals(eventSource)) return SQS_FUN;
+                else return KINESIS_FUN;
+            } catch (Exception e) {
+                String msg = "Failed to find suitable function for incoming event";
+                throw new RuntimeException(msg, e);
+            }
+        }
     }
 }
--- src/test/java/com/example/springcloudbug/SpringCloudBugApplicationTests.java
+++ src/test/java/com/example/springcloudbug/SpringCloudBugApplicationTests.java
@@ -1,16 +1,57 @@
 package com.example.springcloudbug;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SpringCloudBugApplicationTests {
 
-    @Test
-    void contextLoads() {
+    private static byte[] kinesisJson;
+    private static byte[] sqsJson;
+
+    private static FunctionInvoker functionInvoker;
+
+    @BeforeAll
+    static void init() throws Exception {
         System.setProperty("MAIN_CLASS", SpringCloudBugApplication.class.getCanonicalName());
-        FunctionInvoker functionInvoker = new FunctionInvoker();
+        functionInvoker = new FunctionInvoker();
         System.clearProperty("MAIN_CLASS");
+
+
+        kinesisJson =
+                FileCopyUtils.copyToByteArray(
+                        SpringCloudBugApplicationTests.class.getResourceAsStream(
+                                "/validKinesis.json"));
+        sqsJson =
+                FileCopyUtils.copyToByteArray(
+                        SpringCloudBugApplicationTests.class.getResourceAsStream(
+                                "/validSqs.json"));
+
     }
+
+
+    @Test
+    void sqsRequestProcessedSuccessfully() throws IOException {
+        ByteArrayOutputStream outputSqs = new ByteArrayOutputStream();
+        functionInvoker.handleRequest(new ByteArrayInputStream(sqsJson), outputSqs, null);
+        assertEquals(SpringCloudBugApplication.SQS_FUN, outputSqs.toString(UTF_8));
+    }
+
+    @Test
+    void kinesisrequestsProcessedSuccessfully() throws IOException {
+        ByteArrayOutputStream outputKinesis = new ByteArrayOutputStream();
+        functionInvoker.handleRequest(new ByteArrayInputStream(kinesisJson), outputKinesis, null);
+        assertEquals("\""+SpringCloudBugApplication.KINESIS_FUN+"\"", outputKinesis.toString(UTF_8));
+    }
+
 
 }
